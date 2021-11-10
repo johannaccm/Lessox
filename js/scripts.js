fc608b0c7c92ff97ca4ec786952ea976be3e411c
Johanna <jccaraballom@gmail.com>
2021-11-10 18:35:08 -0300

    Avance

---
+++ js/scripts.js
@@ -0,0 +1,66 @@
+
+
+$(function () {
+$(".lupa").click(function() {
+    $(".buscador").show("fast");
+});
+
+$(".close").click(function(){
+    $(".buscador").hide("fast");
+});
+
+$(".dropdown").hover(function(){ 
+    $(this).find(".dropdown-content").toggle(); 
+});
+
+})
+
+
+// LOGO
+var logo = new Vue({
+    el: '.logo',
+    data: {
+        logoLessox: './images/lessox-logo.png',
+        url: 'https://compratucelu.cl/',
+    },
+
+})
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block"; 
+  dots[slideIndex-1].className += " active";
+}
+
+$(function () { 
+
+    $(".navbar-toggler").blur(function (event) {
+      var screenWidth = window.innerWidth;
+      if (screenWidth < 768) {
+        $(".navbar-collapse").collapse('hide');
+      }
+    });
+});
